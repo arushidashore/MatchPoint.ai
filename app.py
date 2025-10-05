df13f3af3021773c34ce9a71c7acc78a7f1398a3
Arushi Dashore <arushi.dashore@gmail.com>
2025-10-04 18:39:46 -0700

    port change to 5000

--- app.py
+++ app.py
@@ -476,5 +476,5 @@ if __name__ == '__main__':
         db.create_all()
     
     # Run the app
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
