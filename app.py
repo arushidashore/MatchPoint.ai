7178b0518fe9689d21056638f3acbb68053d8cb6
Arushi Dashore <arushi.dashore@gmail.com>
2025-10-04 19:54:20 -0700

    test

--- app.py
+++ app.py
@@ -78,7 +78,6 @@ class Badge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.String(200), nullable=False)
-
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
